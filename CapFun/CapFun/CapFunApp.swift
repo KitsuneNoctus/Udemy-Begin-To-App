0e01f9b4ebca99f95254f4101ddeca4d86b62621
KitsuneNoctus <theceintiant@gmail.com>
2024-07-01 17:17:53 -0700

    New project

---
+++ CapFun/CapFun/CapFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapFunApp.swift
+//  CapFun
+//
+//  Created by Henry Calderon on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CapFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
