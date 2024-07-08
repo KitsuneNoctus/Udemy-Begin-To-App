3ca696f3eb15668728d46d58b0beaa8758902651
KitsuneNoctus <theceintiant@gmail.com>
2024-07-08 15:33:18 -0700

    Just adding and toggling a sheet

---
+++ DayJournal/DayJournal/CreateJournalEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateJournalEntryView.swift
+//  DayJournal
+//
+//  Created by Henry Calderon on 7/8/24.
+//
+
+import SwiftUI
+
+struct CreateJournalEntryView: View {
+    var body: some View {
+        Text("Create Journal Entry")
+    }
+}
+
+#Preview {
+    CreateJournalEntryView()
+}
--- DayJournal/DayJournal/JournalEntriesList.swift
+++ DayJournal/DayJournal/JournalEntriesList.swift
@@ -9,9 +9,12 @@ import SwiftUI
 import SwiftData
 
 struct JournalEntriesList: View {
-    @Environment(\.modelContext) private var modelContext
     
+    @Environment(\.modelContext) private var modelContext
+
     @Query private var journalEntries: [JournalEntry]
+    
+    @State var showCreateView = false
     
     var body: some View {
         NavigationStack {
@@ -22,15 +25,23 @@ struct JournalEntriesList: View {
             }
             .navigationTitle("\(journalEntries.count) Journal Entries")
             .toolbar {
-                Button(action: addItem) {
-                    Label("Add Item", systemImage: "plus")
-                }
+//                Button(action: addItem) {
+//                    Label("Add Item", systemImage: "plus")
+//                }
 //                Button {
 //                    let newJournalEntry = JournalEntry(title: "Made a Journal Entry", text: "Used SwiftData to make and save a Journal Entry", rating: 4, date: Date())
 //                    modelContext.insert(newJournalEntry)
 //                } label: {
 //                    Label("Add Item", systemImage: "plus")
 //                }
+                Button {
+                    showCreateView = true
+                } label: {
+                    Label("Add Item", systemImage: "plus")
+                }
+            }
+            .sheet(isPresented: $showCreateView) {
+                CreateJournalEntryView()
             }
         }
         
