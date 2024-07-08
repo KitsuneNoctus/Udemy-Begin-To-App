3dfccb74bfc4bfc82f76ab1864553e242032ca12
KitsuneNoctus <theceintiant@gmail.com>
2024-07-08 09:05:37 -0700

    Starting Section 4

---
+++ DayJournal/DayJournal/DayJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayJournalApp.swift
+//  DayJournal
+//
+//  Created by Henry Calderon on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DayJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
