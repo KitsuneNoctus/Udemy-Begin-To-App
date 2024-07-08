78acdc9392ddc417458b9db6d8f695fcd31b239e
KitsuneNoctus <theceintiant@gmail.com>
2024-07-08 14:24:25 -0700

    Swift Data Basics start

---
+++ DataPractice/DataPractice/DataPracticeApp.swift
@@ -0,0 +1,32 @@
+//
+//  DataPracticeApp.swift
+//  DataPractice
+//
+//  Created by Henry Calderon on 7/8/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DataPracticeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DataPractice/DataPractice/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DataPractice
+//
+//  Created by Henry Calderon on 7/8/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
