6ba3a692ac1992690b8e69b3d2eb5ed349b93537
KitsuneNoctus <theceintiant@gmail.com>
2024-07-09 12:17:06 -0700

    CourseList, last app of course, start

---
+++ CourseList/CourseList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CourseList
+//
+//  Created by Henry Calderon on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CourseList/CourseList/CourseListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseListApp.swift
+//  CourseList
+//
+//  Created by Henry Calderon on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CourseListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
